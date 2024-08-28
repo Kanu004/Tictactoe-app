64b8878a299dba3ec0eccb2d2745e7dcfd6be2c8
kanika <kanika@gmail.com>
2024-08-28 21:51:18 +0530

    adding image in app page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import  './styles.scss';
-
+import images from './Com 512.png';
+import image from './Two 512.png';
 const App = () => {
   return (
-  <div className="app">
-      <h1>
+  <div>
+      <h1 className='class'>
         TIC <span className="text-green">TAC</span> TOE
-      </h1>
+      </h1>  
+       <img className="buttons" src={images} alt='imagee'/> 
+       <img className="buttons" src={image} alt='imagee'/> 
       </div>
   );
+  
 }
+
+
 export default App;
